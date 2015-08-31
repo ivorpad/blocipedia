7489e7acae87cab354d87bae0db8af0d790ec526
Ivor Padilla <ivorjpc@gmail.com>
2015-08-31 12:52:47 -0430

    only users with premium and admin capabilities can see and edit private wikis

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,6 +2,11 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
 
   validates :title, length: { minimum: 5 }, presence: true
+  after_initialize :set_default_private_option, :if => :new_record?
+
+  def set_default_private_option
+    self.private ||= false
+  end
 
   # Added to avoid the violation of The Law of Demeter
   # Now we can use @wiki.username to display the username in a view.
